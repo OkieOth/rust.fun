fa26603c95bac7d45e609e95a4d8e3e824988dff
Eiko Thomas <eiko.thomas@gmail.com>
2023-05-17 00:23:12 +0200

    wip: permutations

--- permutations/src/main.rs
+++ permutations/src/main.rs
@@ -11,14 +11,88 @@ fn print_usage_and_exit(program_name: &str) {
 }
 
 fn main() {
-    if env::args().len() == 1 {
+    if env::args().len() != 2 {
         let args: Vec<String> = env::args().collect();
         print_usage_and_exit(&args[0]);
     }
-    for arg in env::args() {
-        println!("  command line parameter: {}", arg);
+    let args: Vec<String> = env::args().collect();
+    let digit_str = &args[1];
+
+
+    // Attempt to parse the string as an integer
+    match digit_str.parse::<u32>() {
+        Ok(digit_int) => {
+            println!("I am going to create permutations for {} digits ...", digit_int);
+            print_permutions(digit_int)
+        },
+        Err(_) => {
+            println!("Failed to parse the string as an unsigned integer");
+            process::exit(1);
+        }
     }
 }
+
+fn create_permutation_entry(number_count: u32) -> Vec<u32> {
+    let mut ret: Vec<u32> = Vec::new();
+    for i in 0..number_count {
+        ret.push(i);
+    }
+    ret
+}
+
+fn permutations(input: Vec<u32>) -> Vec<Vec<u32>> {
+	let inputLen = input.len();
+	if inputLen == 2 {
+        let mut ret: Vec<Vec<u32>> = Vec::new();
+        ret.push(input.clone());
+        let mut p: Vec<u32> = Vec::new();
+        p.push(input[1]);
+        p.push(input[0]);
+        ret.push(p);
+        ret
+	} else {
+        let ret: Vec<Vec<u32>> = Vec::new();
+        ret
+    }
+    
+/*    
+     else {
+		ret := createPermutationsArray(inputLen)
+		permCount_0 := fakultaet(inputLen - 1)
+		for i := 0; i < inputLen; i++ {
+			restSlice := getRestSlice(input, i)
+			restPermutations := permutations(restSlice)
+			for j := 0; j < permCount_0; j++ {
+				index := (i * permCount_0) + j
+				ret[index][0] = input[i]
+				for k := 1; k < inputLen; k++ {
+					ret[index][k] = restPermutations[j][k-1]
+				}
+			}
+		}
+		return ret
+	}
+*/
+}
+
+fn permutation_to_str(elem: &Vec<u32>) -> String {
+    let mut ret = String::new();
+    for e in elem.iter() {
+        ret.push_str(&e.to_string())
+    }
+    ret
+}
+
+fn print_permutions(number_count: u32) {
+	let input = create_permutation_entry(number_count);
+    
+	let permutations_vector = permutations(input);
+    println!("Number of permutations: {}\n\n", permutations_vector.len());
+	for (i,elem) in permutations_vector.iter().enumerate() {
+		println!("{}: {}", i, permutation_to_str(&elem))
+	}
+}
+
 
 fn factorial(n: u64) -> u64 {
     if n == 0 {
@@ -37,11 +111,6 @@ fn test_factorial() {
     assert_eq!(factorial(5), 120);
     assert_eq!(factorial(6), 720);
     assert_ne!(factorial(6), 721);
-}
-
-fn permutations(input: Vec<u64>) -> Vec<Vec<u64>> {
-    // TODO
-    return Vec::new();
 }
 
 
