b6101930c51f634d69701f20ec738e48d773de59
Eiko Thomas <eiko.thomas@gmail.com>
2023-05-29 20:48:06 +0200

    JSON parsing

--- 05_json/src/main.rs
+++ 05_json/src/main.rs
@@ -4,6 +4,8 @@ use std::io::{BufReader, BufRead};
 use std::collections::HashMap;
 use std::any::type_name;
 use serde_json;
+use serde::Deserialize;
+use uuid::Uuid;
 
 use crate::serde_json::Value;
 
@@ -11,14 +13,26 @@ fn type_of<T>(_: T) -> &'static str {
     type_name::<T>()
 }
 
+#[derive(Debug, Deserialize)]
+struct SimpleJsonCont {
+    #[serde(rename = "b")]
+    b_value: Option<bool>,
+    #[serde(rename = "string")]
+    string_value: Option<String>,
+    #[serde(rename = "zahl")]
+    zahl_value: Option<f32>,
+    #[serde(rename = "uuid")]
+    uuid_value: Option<Uuid>,
+}
+
 fn read_file_as_string(file_name: &str) -> std::io::Result<()> {
     //let error_msg = format!("Can't read the desired file: {}", file_name);
     match fs::read_to_string(file_name) {
         Ok(s) => {
             //println!("file content: \n{s}");
-            let jsonContent: HashMap<String, serde_json::Value> = serde_json::from_str(&s).expect("Error while deserialize JSON");
-            for key in jsonContent.keys() {
-                match jsonContent.get(key) {
+            let json_content: HashMap<String, serde_json::Value> = serde_json::from_str(&s).expect("Error while deserialize JSON");
+            for key in json_content.keys() {
+                match json_content.get(key) {
                     Some(v) => {
                         let st :&str;
                         match v {
@@ -36,7 +50,10 @@ fn read_file_as_string(file_name: &str) -> std::io::Result<()> {
             }
             return Ok(())
         },
-        Err(e) => return Err(e),
+        Err(e) => {
+            println!("Error while reading file: {}", file_name);
+            return Err(e)
+        },
     }
 }
 
@@ -46,9 +63,31 @@ fn read_file_with_buffer(file_name: &str) -> std::io::Result<()> {
         Err(error) => return Err(error),
     };
     let reader = BufReader::new(file);
+    let mut s: String = String::from("");
     for line in reader.lines() {
         let line = line?;
-        println!("{}", line);
+        //println!("{}", line);
+        s = format!("{}{}", s, line);
+    }
+    let json_cont: Vec<SimpleJsonCont> = serde_json::from_str(&s).unwrap();
+    print!("I deserialized {} elems.\n", json_cont.len());
+    let mut i: u8 = 0;
+    for j in json_cont {
+        print!("Object #{}: \n", i);
+        if j.b_value.is_some() {
+            print!("    b_value: {}\n", j.b_value.unwrap());
+        }
+        if j.string_value.is_some() {
+            print!("    string_value: {}\n", j.string_value.unwrap());
+        }
+        if j.zahl_value.is_some() {
+            print!("    zahl_value: {}\n", j.zahl_value.unwrap());
+        }
+        if j.uuid_value.is_some() {
+            print!("    uuid_value: {}\n", j.uuid_value.unwrap());
+        }
+        print!("\n");
+        i = i+1;
     }
     Ok(())
 }
@@ -57,7 +96,15 @@ fn read_file_with_buffer(file_name: &str) -> std::io::Result<()> {
 fn main() ->  std::io::Result<()> {
     let file_name = "resources/yacg_model_schema.json";
     read_file_as_string(file_name).expect("error in read_file_as_string");
-    //read_file_with_buffer(file_name).expect("error in read_file_with_buffer");
+    let file_name2 = "resources/simple.json";
+    read_file_with_buffer(file_name2).expect("error in read_file_with_buffer");
+    let file_name3 = "resources/simple2.json";
+    read_file_with_buffer(file_name3).expect("error in read_file_with_buffer");
+    let file_name4 = "resources/simple2222.json";
+    match read_file_as_string(file_name4) {
+        Ok(_) => println!("Would have expect an error in reading not existing files :-/"),
+        Err(_) => println!("Got expected error :)"),
+    }
     Ok(())
 }
 
